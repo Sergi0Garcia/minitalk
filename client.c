28d3bbd3ccb48b79b18a82a05bd11a8a11b7c6d0
Sergio Garcia <segarcia@student.42heilbronn.de>
2022-10-10 11:17:58 +0200

    Validation for numbers

--- client.c
+++ client.c
@@ -6,13 +6,14 @@
 /*   By: segarcia <segarcia@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/03 10:28:31 by segarcia          #+#    #+#             */
-/*   Updated: 2022/10/06 13:26:28 by segarcia         ###   ########.fr       */
+/*   Updated: 2022/10/10 11:15:38 by segarcia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <signal.h>
 #include "./libft/libft.h"
+#include <stdio.h>
 
 static void	send_char(int pid, char current_char)
 {
@@ -28,6 +29,32 @@ static void	send_char(int pid, char current_char)
 		usleep(200);
 		current_bit++;
 	}
+}
+
+static int	valid_signal(char *str)
+{
+	int	i;
+	int	num;
+
+	i = 0;
+	num = 0;
+	while (str[i])
+	{
+		if (!ft_isdigit(str[i]))
+			return (0);
+		num = (num * 10) + (str[i] - 48);
+		i++;
+	}
+	if (num > 0 && num < 30)
+		return (num);
+	else
+		return (0);
+}
+
+static int	return_int_signal(int pid, int signal)
+{
+	kill(pid, signal);
+	return (0);
 }
 
 void	arg_val(int argc, char **argv)
@@ -57,8 +84,11 @@ int	main(int argc, char **argv)
 	i = 0;
 	arg_val(argc, argv);
 	pid = ft_atoi(argv[1]);
+	if (valid_signal(argv[2]))
+		return (return_int_signal(pid, valid_signal(argv[2])));
 	str = argv[2];
 	while (str[i])
 		send_char(pid, str[i++]);
+	send_char(pid, '\n');
 	return (0);
 }
